a7df1588200c7695e49d746a34b85b32c1804b81
bisuMei <bisumeikp@gmail.com>
2020-10-31 16:48:09 -0700

    Add profile form

--- training/forms.py
+++ training/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User, Group
-from django.forms import ModelForm, TextInput, Textarea, DateTimeInput
+from django.forms import ModelForm, TextInput, Textarea, DateTimeInput, NumberInput, DateInput, ImageField
 
 from .models import Workout, Comment, Profile
 
@@ -45,5 +45,20 @@ class CommentForm(ModelForm):
         fields = ['name', 'body']
 
 
+class ProfileForm(ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['birth', 'height', 'weight', 'chest_volume',
+                  'waist', 'arm_volume', 'hip_volume', 'photo']
+        widgets = {
+            'birth': DateInput(attrs={'class': 'form-control'}),
+            'height': NumberInput(attrs={'class': 'form-control'}),
+            'weight': NumberInput(attrs={'class': 'form-control'}),
+            'chest_volume': NumberInput(attrs={'class': 'form-control'}),
+            'waist': NumberInput(attrs={'class': 'form-control'}),
+            'arm_volume': NumberInput(attrs={'class': 'form-control'}),
+            'hip_volume': NumberInput(attrs={'class': 'form-control'}),
+
+        }
 
 
